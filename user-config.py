ee22b830723061e7592e09c3079d42ce16df583c
Helloeveryone-ctrl <reefcoral810@gmail.com>
2025-08-03 18:33:17 +0800

    Create user-config.py

---
+++ user-config.py
@@ -0,0 +1,14 @@
+family = 'wikipedia'
+mylang = 'test'
+
+usernames = {
+    'wikipedia': {
+        'test': 'Cactusisme',  # Your bot username
+    }
+}
+
+password_file = {
+    'wikipedia': {
+        'test': 'YourBotPasswordHere'  # Your bot password (plain text)
+    }
+}
